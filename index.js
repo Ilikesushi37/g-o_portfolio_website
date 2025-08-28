af202d9299290401d4a3e127ab457b10adfdd050
Kylania Saeteurn <76223982+Ilikesushi37@users.noreply.github.com>
2025-08-27 20:37:44 -0700

    finalizing the javascript file, with the background changers

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const peilinSection = document.getElementsByClassName("peilin");
 const kylaniaSection = document.getElementsByClassName("kylania");
 const chrisSection = document.getElementsByClassName("chris");
 const imaniSection = document.getElementsByClassName("imani");
+
 marvinsection.addEventListener("click", function() {
     marvinsection.style.backgroundcolor = rgb(22, 255, 205);
 });
 
 peilinSection.addEventListener("click", function() {
-    peilinSection.style.backgroundcolor = 'lightyellow'; 
+    peilinSection.style.backgroundcolor = "lightyellow"; 
 });
 
+kylaniaSection.addEventListener("click", function() {
+    kylaniaSection.style.backgroundcolor = rgba(113, 202, 232, 1);
+});
 
+chrisSection.addEventListener("click", function() {
+    chrisSection.style.backgroundcolor = rgb(189, 25, 25);
+});
+
+imaniSection.addEventListener("click", function() {
+    imaniSection.style.backgroundcolor = "purple";
+});
