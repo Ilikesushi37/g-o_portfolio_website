34ec8a6c4f945638b0c568e1c761f93c939f5552
Kylania Saeteurn <76223982+Ilikesushi37@users.noreply.github.com>
2025-08-27 20:31:44 -0700

    re adding the const vars because I accidendatlly deleted it

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const marvinSection = document.getElementsByClassName("marvin");
+const peilinSection = document.getElementsByClassName("peilin");
+const kylaniaSection = document.getElementsByClassName("kylania");
+const chrisSection = document.getElementsByClassName("chris");
+const imaniSection = document.getElementsByClassName("imani");
 marvinsection.addEventListener("click", function() {
-marvinsection.style.backgroundcolor = rgb(22, 255, 205);
-}
-);
+    marvinsection.style.backgroundcolor = rgb(22, 255, 205);
+});
+
+peilinSection.addEventListener("click", function() {
+    peilinSection.style.backgroundcolor = 'lightyellow'; 
+});
+
+
