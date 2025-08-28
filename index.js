54428024e51a0addc8388737e6ab37a79e83d59e
Kylania Saeteurn <76223982+Ilikesushi37@users.noreply.github.com>
2025-08-27 19:58:42 -0700

    working on add event listener

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-const kylaniaSection = document.getElementsByClassName("kylania");
-
+const marvinSection = document.getElementsByClassName("marvin");
+marvinsection.addEventListener("click", function() {
+marvinsection.style.backgroundcolor = rgb(22, 255, 205);
+}
+);
