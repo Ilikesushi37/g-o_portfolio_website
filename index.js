f41f6c357dd4c432a8302e9086fbd18a48acbb00
Kylania Saeteurn <76223982+Ilikesushi37@users.noreply.github.com>
2025-08-27 20:42:12 -0700

    somethigns nawt workigng, maybe it's because I forgot to capitalize the C

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const chrisSection = document.getElementsByClassName("chris");
 const imaniSection = document.getElementsByClassName("imani");
 
 marvinsection.addEventListener("click", function() {
-    marvinsection.style.backgroundcolor = rgb(22, 255, 205);
+    marvinsection.style.backgroundColor = rgb(22, 255, 205);
 });
 
 peilinSection.addEventListener("click", function() {
-    peilinSection.style.backgroundcolor = "lightyellow"; 
+    peilinSection.style.backgroundColor = 'lightyellow'; 
 });
 
 kylaniaSection.addEventListener("click", function() {
-    kylaniaSection.style.backgroundcolor = rgba(113, 202, 232, 1);
+    kylaniaSection.style.backgroundColor = rgba(113, 202, 232, 1);
 });
 
 chrisSection.addEventListener("click", function() {
-    chrisSection.style.backgroundcolor = rgb(189, 25, 25);
+    chrisSection.style.backgroundColor = rgb(189, 25, 25);
 });
 
 imaniSection.addEventListener("click", function() {
-    imaniSection.style.backgroundcolor = "purple";
+    imaniSection.style.backgroundColor = 'purple';
 });
